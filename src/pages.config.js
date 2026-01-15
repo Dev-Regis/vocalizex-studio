a8711e73173947dc2d3010aac8a9b091ffdfc6c9
Regi <mundodigital764@gmail.com>
2026-01-15 21:14:12 +0000

    Gerador de Letras JavaScript

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,18 +1,18 @@
 import BatchImages from './pages/BatchImages';
+import Home from './pages/Home';
 import ImageChat from './pages/ImageChat';
 import ImageCreator from './pages/ImageCreator';
 import LyricGenerator from './pages/LyricGenerator';
 import LyricsImage from './pages/LyricsImage';
-import Home from './pages/Home';
 
 
 export const PAGES = {
     "BatchImages": BatchImages,
+    "Home": Home,
     "ImageChat": ImageChat,
     "ImageCreator": ImageCreator,
     "LyricGenerator": LyricGenerator,
     "LyricsImage": LyricsImage,
-    "Home": Home,
 }
 
 export const pagesConfig = {
